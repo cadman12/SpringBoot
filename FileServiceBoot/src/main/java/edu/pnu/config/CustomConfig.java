33dcdf0df35aa0438110e2b98fb671a05e0aa6d1
Lee, Sanghyun <65234370+cadman12@users.noreply.github.com>
2024-09-06 08:56:25 +0900

    Update CustomConfig.java

--- FileServiceBoot/src/main/java/edu/pnu/config/CustomConfig.java
+++ FileServiceBoot/src/main/java/edu/pnu/config/CustomConfig.java
@@ -13,7 +13,8 @@ public class CustomConfig implements WebMvcConfigurer {
 		// http://localhost:8080/images/123.png 같은 형식의 이미지 요청시 처리 핸들러 등록
 		
 		// jar을 생성할 때 이미 저장된 리소스 호출
-		// classpath ==> .jar/BOOT-INF/classes/
+		// classpath ==> .jar/BOOT-INF/classes/ 를 의미
+		// 		이클립스 Package Explorer에서는 src/main/resources를 의미
 		registry.addResourceHandler("/images/**")
 			.addResourceLocations("classpath:static/images/","file:c:/Temp/upload/");
 
