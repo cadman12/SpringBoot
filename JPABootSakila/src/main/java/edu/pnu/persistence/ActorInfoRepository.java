5073420205a24d4dabf5a97543248431cb6ae9aa
Lee, Sanghyun <geodb@daum.net>
2024-10-14 15:33:28 +0900

    2024-10-14

--- JPABootSakila/src/main/java/edu/pnu/persistence/ActorInfoRepository.java
+++ JPABootSakila/src/main/java/edu/pnu/persistence/ActorInfoRepository.java
@@ -10,9 +10,34 @@ import edu.pnu.domain.ActorInfo;
 public interface ActorInfoRepository extends JpaRepository<ActorInfo, Integer> {
 
 	List<ActorInfo> findByFirstNameContainingOrLastNameContaining(String firstName, String lastName);
-	
+
 	@Procedure("film_in_stock1")
-	Integer filmInStock1(Integer p_film_id, Integer p_store_id);
+	List<Object[]> filmInStock1();
+//  입력, 출력 파라미터가 없고 질의만 호출하는 경우
+//	CREATE DEFINER=`root`@`localhost` PROCEDURE `film_in_stock1`()
+//	    READS SQL DATA
+//	BEGIN
+//	     SELECT inventory_id, film_id, store_id
+//	     FROM inventory
+//	     WHERE film_id = 1;
+//	END
+
+	@Procedure("film_in_stock2")
+	List<Object[]> filmInStock2(Integer p_film_id, Integer p_store_id);
+//  1개 이상의 입력 파라미터가 있고, 출력 파라미터 없이 호출하는 경우
+//	CREATE DEFINER=`root`@`localhost` PROCEDURE `film_in_stock1`(IN p_film_id INT, IN p_store_id INT)
+//		READS SQL DATA
+//	BEGIN
+//		SELECT inventory_id, film_id, store_id
+//		FROM inventory
+//		WHERE film_id = p_film_id
+//		AND store_id = p_store_id
+//		AND inventory_in_stock(inventory_id);
+//	END
+
+	@Procedure("film_in_stock3")
+	Integer filmInStock3(Integer p_film_id, Integer p_store_id);
+//  1개 이상 input 파라미터, 1개의 output 파라미터 ==> output 파라미터는 메서드의 리턴값으로 변환된다.
 //	CREATE DEFINER=`root`@`localhost` PROCEDURE `film_in_stock1`(IN p_film_id INT, IN p_store_id INT, OUT p_film_count INT)
 //		READS SQL DATA
 //	BEGIN
@@ -23,19 +48,49 @@ public interface ActorInfoRepository extends JpaRepository<ActorInfo, Integer> {
 //		AND inventory_in_stock(inventory_id)
 //		INTO p_film_count;
 //	END
-	
-	@Procedure("film_in_stock2")
-	List<Object[]> filmInStock2(Integer p_film_id, Integer p_store_id);
-//	CREATE DEFINER=`root`@`localhost` PROCEDURE `film_in_stock2`(IN p_film_id INT, IN p_store_id INT)
-//		READS SQL DATA
+
+//	@Procedure("film_in_stock4")
+//	List<Object> filmInStock4(Integer p_film_id, Integer p_store_id);
+//  1개 이상 input 파라미터, 2개 이상 output 파라미터 ==> JPA에서는 복수의 output 파라미터는 지원하지 않음.
+//	==> StoredProcedureQuery 객체를 이용해서 처리함. 
+//	CREATE DEFINER=`root`@`localhost` PROCEDURE `film_in_stock4`(IN p_film_id INT, IN p_store_id INT, OUT p_film_count INT, OUT sum INT)
+//	    READS SQL DATA
+//	BEGIN
+//	     SELECT COUNT(*)
+//	     FROM inventory
+//	     WHERE film_id = p_film_id
+//	     AND store_id = p_store_id
+//	     AND inventory_in_stock(inventory_id)
+//	     INTO p_film_count;
+//		     
+//	     SELECT sum(inventory_id)
+//	     FROM inventory
+//	     WHERE film_id = p_film_id
+//	     AND store_id = p_store_id
+//	     AND inventory_in_stock(inventory_id)
+//	     INTO sum;     
+//	END
+
+//	@Procedure("film_in_stock")
+//	List<Object> filmInStock(Integer p_film_id, Integer p_store_id);
+//  1개 이상 input 파라미터, 1개 이상 output 파라미터와 질의 결과 셋이 있는 경우
+//	==> 복수개의 질의가 연속될 경우 제일 첫번째 질의의 결과셋이 리턴된다.
+//	==> JPA에서는 복수의 output 파라미터는 지원하지 않음.
+//	==> StoredProcedureQuery 객체를 이용해서 처리함. 
+//	CREATE DEFINER=`root`@`localhost` PROCEDURE `film_in_stock`(IN p_film_id INT, IN p_store_id INT, OUT p_film_count INT)
+//	    READS SQL DATA
 //	BEGIN
 //	    SELECT inventory_id
-//		FROM inventory
-//		WHERE film_id = p_film_id
-//		AND store_id = p_store_id
-//		AND inventory_in_stock(inventory_id);
-//		     
-//		SELECT *
-//		FROM actor;
-//	END
+//	    FROM inventory
+//	    WHERE film_id = p_film_id
+//	    AND store_id = p_store_id
+//	    AND inventory_in_stock(inventory_id);
+//	
+//	    SELECT COUNT(*)
+//	    FROM inventory
+//	    WHERE film_id = p_film_id
+//	    AND store_id = p_store_id
+//	    AND inventory_in_stock(inventory_id)
+//	    INTO p_film_count;	     
+//	END	
 }
--- JPABootSakila/src/test/java/edu/pnu/StoredProcedureTest.java
+++ JPABootSakila/src/test/java/edu/pnu/StoredProcedureTest.java
@@ -12,6 +12,7 @@ import jakarta.persistence.ParameterMode;
 import jakarta.persistence.StoredProcedureQuery;
 import jakarta.transaction.Transactional;
 
+@Transactional
 @SpringBootTest
 class StoredProcedureTest {
 
@@ -20,38 +21,25 @@ class StoredProcedureTest {
 	
 	@Autowired
 	private EntityManager entityManager;
-	
-//	@Test
-	void test01() {
-		// 저장 프로시저 실행 객체 생성
-		StoredProcedureQuery query = entityManager.createStoredProcedureQuery("film_in_stock");
-		
-		// 프로시저 파라미터 등록
-		query.registerStoredProcedureParameter("p_film_id", Integer.class, ParameterMode.IN);
-		query.registerStoredProcedureParameter("p_store_id", Integer.class, ParameterMode.IN);
-		query.registerStoredProcedureParameter("p_film_count", Integer.class, ParameterMode.OUT);
-		
-		// 입력값 설정
-		query.setParameter("p_film_id", 1);
-		query.setParameter("p_store_id", 1);
-		
-		// 저장 프로시저 실행
-		query.execute();
-		
-		// 결과값 추출
-		System.out.println(query.getOutputParameterValue("p_film_count"));
-	}
 
 	@Test
-	void test02() {
-		System.out.println("test02:" + aiRepo.filmInStock1(1,  1));
+	void test01() {
+		
+		System.out.println("test01:입력, 출력 파라미터가 없고 질의만 호출하는 경우");
+
+		List<Object[]> res = aiRepo.filmInStock1();
+		for (Object[] obj : res) {
+			for (Object o : obj) {
+				System.out.print(o.toString() + ", ");
+			}
+			System.out.println();
+		}
 	}	
 	
-	@Transactional
 	@Test
-	void test03() {
+	void test02() {
 		
-		System.out.println("test03");
+		System.out.println("test02:1개 이상의 입력 파라미터가 있고, 출력 파라미터 없이 호출하는 경우");
 
 		List<Object[]> list = aiRepo.filmInStock2(1, 1);
 
@@ -59,4 +47,84 @@ class StoredProcedureTest {
 			System.out.println(objs);
 		}
 	}
+	
+	@Test
+	void test03() {
+		
+		System.out.println("test03:1개 이상 input 파라미터, 1개의 output 파라미터 ==> output 파라미터는 메서드의 리턴값으로 변환된다.");
+
+		int res = aiRepo.filmInStock3(1, 1);
+		System.out.println(res);
+	}
+	
+	@Test
+	void test04() {
+		
+		System.out.println("test04:1개 이상 input 파라미터, 2개 이상 output 파라미터");
+
+		StoredProcedureQuery query = entityManager.createStoredProcedureQuery("film_in_stock4");
+        
+        // IN 매개변수 설정
+        query.registerStoredProcedureParameter("p_film_id", Integer.class, ParameterMode.IN);
+        query.registerStoredProcedureParameter("p_store_id", Integer.class, ParameterMode.IN);
+        
+        // OUT 매개변수 설정
+        query.registerStoredProcedureParameter("p_film_count", Integer.class, ParameterMode.OUT);
+        query.registerStoredProcedureParameter("sum", Integer.class, ParameterMode.OUT);
+        
+        // 매개변수 값 설정
+        query.setParameter("p_film_id", 1);
+        query.setParameter("p_store_id", 1);
+
+        // 실행
+        query.execute();
+
+        // OUT 매개변수 값 가져오기
+        Integer p_film_count = (Integer) query.getOutputParameterValue("p_film_count");
+        Integer sum = (Integer) query.getOutputParameterValue("sum");
+
+        System.out.println("p_film_count:" + p_film_count + ", sum:" + sum);
+	}
+	
+	@Test
+	void test05() {
+		
+		System.out.println("test05:1개 이상 input 파라미터, 1개 이상 output 파라미터, 질의 결과 셋");
+
+		StoredProcedureQuery query = entityManager.createStoredProcedureQuery("film_in_stock");
+        
+        // IN 매개변수 설정
+        query.registerStoredProcedureParameter("p_film_id", Integer.class, ParameterMode.IN);
+        query.registerStoredProcedureParameter("p_store_id", Integer.class, ParameterMode.IN);
+        
+        // OUT 매개변수 설정
+        query.registerStoredProcedureParameter("p_film_count", Integer.class, ParameterMode.OUT);
+        
+        // 매개변수 값 설정
+        query.setParameter("p_film_id", 1);
+        query.setParameter("p_store_id", 1);
+
+        // 실행
+        boolean hasResultSet = query.execute();
+
+        // OUT 매개변수 값 가져오기
+        Integer p_film_count = (Integer) query.getOutputParameterValue("p_film_count");
+
+        System.out.println("p_film_count:" + p_film_count);
+        
+        if (hasResultSet) {
+        	// 질의 결과 셋이 여러 필드인 경우라면 List<Object>가 아닌 List<Object[]>
+        	List<Object> list = query.getResultList();
+    		for (Object obj : list) {
+    			System.out.println(obj);
+    		}
+    		
+    		while(query.hasMoreResults()) {
+    			list = query.getResultList();
+        		for (Object obj : list) {
+        			System.out.println(obj);
+        		}
+            }
+        }
+	}			
 }
