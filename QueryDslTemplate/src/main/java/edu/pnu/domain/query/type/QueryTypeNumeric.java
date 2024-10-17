8fd65a18ff6525f1d11e67c2d19f21e52c70e39e
Lee, Sanghyun <geodb@daum.net>
2024-10-17 15:16:58 +0900

    2024-10-17

--- QueryDslTemplate/src/main/java/edu/pnu/domain/query/type/QueryTypeNumeric.java
+++
@@ -1,8 +0,0 @@
-package edu.pnu.domain.query.type;
-
-public enum QueryTypeNumeric {
-	EQ,	// Equal
-	LT,	// Less Then
-	GT,	// Greater Then
-	BW	// Between
-}
--- QueryDslTemplate/src/main/java/edu/pnu/domain/query/type/QueryTypeString.java
+++
@@ -1,5 +0,0 @@
-package edu.pnu.domain.query.type;
-
-public enum QueryTypeString {
-	EQUAL, LIKE
-}
