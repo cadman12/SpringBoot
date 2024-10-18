4233a3396fdda2de3dfe6e877544f2e4288ed30c
Lee, Sanghyun <geodb@daum.net>
2024-10-18 14:50:16 +0900

    2024-10-18

---
+++ JPAComplexKeyEmbeddedId/src/main/java/edu/pnu/domain/Board.java
@@ -0,0 +1,45 @@
+package edu.pnu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "jpa3_board")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "writer", referencedColumnName = "username"),
+            @JoinColumn(name = "writer_id", referencedColumnName = "member_id")
+    })
+    private Member member;
+    private String image;
+}
